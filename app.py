7c65d692a08d294183bc8d7b39808876eb5f9c32
sakii-ito <ito.sakii.22@gmail.com>
2026-07-05 17:53:43 +0900

    Create first HTML page

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Welcome to Baseball Stats Analyzer!")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
