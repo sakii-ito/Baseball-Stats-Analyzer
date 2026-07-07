46db7270f1b76b0bc4cf057befd47b3a60f9e501
sakii-ito <ito.sakii.22@gmail.com>
2026-07-07 22:36:36 +0900

    Implement batting average calculation

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    average = None
+    if request.method == "POST":
+        
+        player = request.form["player"]
+        at_bats = int(request.form["at_bats"])
+        hits = int(request.form["hits"])
+        
+        if at_bats> 0:
+            average = round(hits / at_bats, 3)
+        else:
+            average = "Cannot divide by 0"
+        
+    return render_template("index.html", average=average)
 
 if __name__ == "__main__":
     app.run(debug=True)
