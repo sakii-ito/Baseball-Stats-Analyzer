96101be874a59bf50e0a683c01b62fe520afd8c6
sakii-ito <ito.sakii.22@gmail.com>
2026-07-10 23:44:48 +0900

    Add On-Base Percentage calculation

--- app.py
+++ app.py
@@ -5,26 +5,37 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def home():
     average = None
+    obp = None
     player = ""
     at_bats = ""
     hits = ""
+    walks = ""
+    
     if request.method == "POST":
         
         player = request.form["player"]
         at_bats = request.form["at_bats"]
         hits = request.form["hits"]
+        walks = request.form["walks"]
         
         if int(at_bats)> 0:
             average = f"{int(hits) / int(at_bats):.3f}"
         else:
             average = "Cannot divide by 0"
         
+        if int(at_bats) + int(walks) > 0:
+            obp = f"{(int(hits) + int(walks)) / (int(at_bats) + int(walks)):.3f}"
+        else:
+            obp = "Cannot divide by 0"
+        
     return render_template(
         "index.html", 
         average=average,
+        obp=obp,
         player=player,
         at_bats=at_bats,
-        hits=hits    
+        hits=hits,
+        walks=walks    
     )
 
 if __name__ == "__main__":
